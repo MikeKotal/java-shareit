6b46006cacdfc4dcb7e4d62f95c1943776874514
Michael Kokotailo <kokotailo2011@yandex.ru>
2024-10-04 20:48:26 +0300

    fixed checkstyle

--- server/src/main/java/ru/practicum/shareit/item/dto/ShortItemDto.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/ShortItemDto.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public interface ShortItemDto {
+
     Long getId();
+
     String getName();
+
     Long getOwnerId();
 }
